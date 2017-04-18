8d3e7b50c33246e08bc38a320f8f65cb1ceedb4a
Anh <quanganh.tran94@gmail.com>
2017-04-18 22:14:31 +0300

    Copy game assets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,3 @@
 object Main extends App {
-  Controller()
-}
 
-object Controller {
-  private var currentScreen: Screen = _
-
-  def apply(): Unit = {
-    val inn = Screen("Inn", Map())
-    toScreen(inn)
-
-    while (true) {
-      toScreen(currentScreen.onCommand())
-    }
-  }
-
-  def toScreen(target: Screen): Unit = {
-    currentScreen = target
-    currentScreen.reportScreen()
-  }
 }
